3eb89a5eddfd252fafa70509289fe1d5d45c01db
jsharp83 <jsharp83@gmail.com>
2015-11-02 18:43:05 +0900

    no message

--- source/android/OpencvNDKTest/app/src/main/jni/Android.mk
+++ source/android/OpencvNDKTest/app/src/main/jni/Android.mk
@@ -1,6 +1,12 @@
 LOCAL_PATH := $(call my-dir)
     include $(CLEAR_VARS)
-    include ../../
+
+    #opencv
+    OPENCVROOT:= /Users/eunchuljeon/Desktop/DEV_Libraries/OpenCV-android-sdk/
+    OPENCV_CAMERA_MODULES:=off
+    OPENCV_INSTALL_MODULES:=on
+    OPENCV_LIB_TYPE:=SHARED
+    include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
     LOCAL_MODULE    := OpenCVNDKTest
     LOCAL_SRC_FILES := main.cpp
     LOCAL_LDLIBS := -llog
--- source/android/OpencvNDKTest/app/src/main/jni/main.cpp
+++ source/android/OpencvNDKTest/app/src/main/jni/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "com_eunchuljeon_opencvndktest_MainActivity.h"
+#include <opencv2/core/core.hpp>
 
 JNIEXPORT jstring JNICALL Java_com_eunchuljeon_opencvndktest_MainActivity_getStringFromNative
 (JNIEnv *env, jobject){
